2af11e17fd7af248854fce0b0822a9b15bacb7bd
Shrey D <shreyder.desai@gmail.com>
2023-09-16 11:29:02 -0400

    Added basic recycler view logic

---
+++ app/src/main/java/com/shreyd/co2tracker/DriveAdapter.kt
@@ -0,0 +1,46 @@
+package com.shreyd.co2tracker
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+
+class DriveAdapter(private val mList: List<Drive>) : RecyclerView.Adapter<DriveAdapter.ViewHolder>() {
+
+    // create new views
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+// inflates the card_view_design view
+// that is used to hold list item
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_recent_drives, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    // binds the list items to a view
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+
+        val itemsViewModel = mList[position]
+
+// sets the image to the imageview from our itemHolder class
+        holder.emit.text = "${itemsViewModel.emission} kg"
+
+// sets the text to the textview from our itemHolder class
+        holder.distance.text = "${itemsViewModel.startLoc} mi"
+
+    }
+
+    // return the number of the items in the list
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+
+    // Holds the views for adding it to image and text
+    class ViewHolder(ItemView: View) : RecyclerView.ViewHolder(ItemView) {
+        val emit: TextView = itemView.findViewById(R.id.emit)
+        val distance: TextView = itemView.findViewById(R.id.distance)
+    }
+}
--- app/src/main/java/com/shreyd/co2tracker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/shreyd/co2tracker/ui/home/HomeFragment.kt
@@ -7,6 +7,8 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.shreyd.co2tracker.Drive
+import com.shreyd.co2tracker.DriveAdapter
 import com.shreyd.co2tracker.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -17,24 +19,30 @@ class HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    val drives = mutableListOf<Drive>()
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //Get Data From Firebase
+        drives.add(Drive("fdsa", listOf(1.2, 23.4), listOf(2.3, 34.5), 1234561, 4321562, emission=16.0))
+        drives.add(Drive("fdksa", listOf(1.2, 23.4), listOf(2.3, 34.5), 1234561, 4321562, emission=16.0))
+
+        val adapter = DriveAdapter(drives)
+        binding.recycler.adapter = adapter
+    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
